d353e66b1eb818714d8e72214cf15ec4630dd787
Mig <mig.isada@gmail.com>
2023-07-22 07:22:40 +0800

    update

--- src/components/Organ.jsx
+++ src/components/Organ.jsx
@@ -1,54 +1,33 @@
 import { Stack } from '@mui/material'
-import React, { useContext, useState, useEffect } from 'react'
+import React, { useRef, useContext, useState, useEffect } from 'react'
 import { EditableText, Button, Collapse, Icon } from "@blueprintjs/core"
 import { IconNames } from "@blueprintjs/icons"
 import NormalOrAbnormal from "./Findings/NormalOrAbnormal"
 import { ReportFindingsContext } from "../contexts/ReportFindingsContext"
+import AddFindings from './Findings/AddFindings'
 
 export default function Organ({ section, modality, onDisplayChange }) {
   const name = section.charAt(0).toUpperCase() + section.slice(1);
-  const [forDisplay, setForDisplay] = useState([""]); 
+  const [forDisplay, setForDisplay] = useState(name + ": "); 
   const [receivedFinding, setReceivedFinding] = useState("");
-  const [normal, setNormal] = useState("");
-  const [isAbnormal, setIsAbnormal] = useState(false); 
   const [isOpen, setIsOpen] = useState(true); 
   const { state: reportFindings, dispatch } = useContext(ReportFindingsContext);
+
+  const textAreaRef = useRef()
+
 
   const onValueChange = (finding) => {
     setReceivedFinding(finding);
   };
 
-  const onNormal = (normalDisplay) => {
-    setNormal(normalDisplay);
-    setIsAbnormal(false);
-  };
-
-  const onAbnormal = () => {
-    setIsAbnormal(true);
-  };
-
-  let defValue = name + ": ";
-  let abnormalDisplay = defValue + forDisplay.join("");
-  let normalDisplay = defValue + normal;
-  let display = normal ? normalDisplay : abnormalDisplay;
-
-  // useEffect(() => {
-  //   if (normal !== "") {
-  //     setIsOpen(false);
-  //   }
-  //   dispatch({ type: "ADD_FINDING", payload: { section, display } });
-  // }, [normal, display, section, dispatch]);
-
   const handleAddClick = () => {
-    setForDisplay((prevForDisplay) => [
-      ...prevForDisplay,
-      receivedFinding,
-      " ",
-    ]); 
+    setForDisplay(prevForDisplay => prevForDisplay + receivedFinding + " ");
   };
 
   const handleRemoveClick = () => {
-    setForDisplay((prevForDisplay) => prevForDisplay.slice(0, -2)); 
+    const findings = forDisplay.trim().split(' ');
+    findings.pop();
+    setForDisplay(findings.join(' ') + ' ');
   };
 
   const handleLockClick = () => {
@@ -56,9 +35,17 @@ export default function Organ({ section, modality, onDisplayChange }) {
   };
 
   useEffect(() => {
-    onDisplayChange(display);
-  }, [display, onDisplayChange]);
- 
+    if (textAreaRef.current) {
+      textAreaRef.current.style.height = 'auto';
+      textAreaRef.current.style.height = textAreaRef.current.scrollHeight + 'px';
+    }
+  }, [forDisplay]);
+
+
+  useEffect(() => {
+    onDisplayChange(forDisplay);
+  }, [forDisplay, onDisplayChange]);
+
   return (
     <div>
       <div style={{ display: "flex", alignItems: "center" }}>
@@ -67,36 +54,50 @@ export default function Organ({ section, modality, onDisplayChange }) {
           minimal={true}
           onClick={handleLockClick}
           style={{ marginRight: '7px' }}
-
         />
         <div style={{ flexGrow: 1 }}>
-          <EditableText
-            value={display}
-            onChange={(value) => setForDisplay([value.slice(defValue.length)])}
+        <textarea
+            ref={textAreaRef}
+            value={forDisplay}
+            onChange={(event) => setForDisplay(event.target.value)}
+            style={{ 
+              width: '100%', 
+              resize: 'none', 
+              border: '1px solid #ced4da', 
+              borderRadius: '.25rem',
+              fontSize: '14px', // Adjust font size
+              fontFamily: 'Arial, sans-serif' // Adjust font family
+            }}
+            disabled={!isOpen}
+          />
+
+          {/* <EditableText
+            value={forDisplay}
+            onChange={(value) => setForDisplay(value)}
             multiline={true}
             maxLines={5}
             style={{ width: '100%'}}
             disabled={!isOpen}
-          />
+          /> */}
         </div>
       </div>
       <Collapse isOpen={isOpen}>
-        <NormalOrAbnormal
-          onValueChange={onValueChange}
-          onNormal={onNormal}
-          onAbnormal={onAbnormal}
-          section={section}
-          modality={modality}
+      <div style={{ padding: '10px' }}> 
+        <AddFindings
+         onValueChange={onValueChange}
+         section={section}
+         modality={modality}
         />
-          <Stack spacing={2} direction="row">
-            <Button intent="primary" onClick={handleAddClick}>
-              Add to report
-            </Button>
-            <Button intent="danger" onClick={handleRemoveClick}>
-              Remove from report
-            </Button>
-          </Stack>
+      </div>
+        <Stack spacing={2} direction="row">
+          <Button intent="primary" onClick={handleAddClick}>
+            Add to report
+          </Button>
+          <Button intent="danger" onClick={handleRemoveClick}>
+            Remove from report
+          </Button>
+        </Stack>
       </Collapse>
     </div>
   )
-}
+} 
